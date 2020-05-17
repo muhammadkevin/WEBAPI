7ef44ddd8b10fd838acd33f47b4f7c007e129f5a
mkevin21 <kodingindie12@gmail.com>
2020-05-18 01:16:42 +0700

    tambah api 2

--- untoldstories/routes/api.php
+++ untoldstories/routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', 'Api\ArtikelController@dashboard');
+Route::get('/artikel', 'Api\ArtikelController@artikel');
