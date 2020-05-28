62756e9d723b422c4ac0028529b8a93a5d7ee8f6
mkevin21 <kodingindie12@gmail.com>
2020-05-29 03:18:28 +0700

    search fix

--- untoldstories/app/Http/Controllers/Api/SearchController.php
+++ untoldstories/app/Http/Controllers/Api/SearchController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Suport\Facades\DB;
+use Illuminate\Support\Facades\DB;
 
 
 class SearchController extends Controller
