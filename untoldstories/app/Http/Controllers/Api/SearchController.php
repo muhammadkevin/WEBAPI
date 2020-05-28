20ade4edd1c127c121ad79e02e5cbf49a6905cf0
mkevin21 <kodingindie12@gmail.com>
2020-05-29 03:22:53 +0700

    search fixs 2

--- untoldstories/app/Http/Controllers/Api/SearchController.php
+++ untoldstories/app/Http/Controllers/Api/SearchController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+use App\Transformer\SearchTransformer;
+
 
 class SearchController extends Controller
 {
