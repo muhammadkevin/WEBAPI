b433da6d4c602548db434d369b41f13f02922cff
mkevin21 <kodingindie12@gmail.com>
2020-05-29 03:15:01 +0700

    search oke

--- untoldstories/app/Http/Controllers/Api/SearchController.php
+++ untoldstories/app/Http/Controllers/Api/SearchController.php
@@ -10,49 +10,46 @@ use Illuminate\Suport\Facades\DB;
 class SearchController extends Controller
 {
     public function search(Request $request){
-       
-        $jadi = "oke";
-        return $jadi;
-        //     $cari = $request->cari;
-    //     $artikel = array();
+        $cari = $request->cari;
+        $artikel = array();
 
-         //berdasarkan judul perkata
-    //     $art = DB::table('artikel')
-    //                 ->join('kategori', 'artikel.kategori_id','=','kategori.id')
-    //                 ->join('foto_blog', 'artikel.id','=','foto_blog.artikel_id')
-    //                 ->where('judul','like',"%".$cari."%")
-    //                 ->orWhere('isi','like',"%".$cari."%")
-    //                 ->orWhere('tanggal','like',"%".$cari."%")->get();
+        //berdasarkan judul perkata
+        $art = DB::table('artikel')
+                    ->join('kategori', 'artikel.kategori_id','=','kategori.id')
+                    ->join('foto_blog', 'artikel.id','=','foto_blog.artikel_id')
+                    ->where('judul','like',"%".$cari."%")
+                    ->orWhere('isi','like',"%".$cari."%")
+                    ->orWhere('tanggal','like',"%".$cari."%")->get();
                      
-    //     if(!empty($art[0]->id)){
-    //         $artikel[] = fractal($art, new SearchTransformer());
-    //     }else{
-    //         $kategori = DB::table('kategori')
-    //             ->join('artikel', 'kategori.id', '=', 'artikel.kategori_id')
-    //             ->join('foto_blog', 'artikel.id', '=', 'foto_blog.artikel_id')
-    //             ->where('kategori.kategori','like',"%".$cari."%")->get();
-    //     }
+        if(!empty($art[0]->id)){
+            $artikel[] = fractal($art, new SearchTransformer());
+        }else{
+            $kategori = DB::table('kategori')
+                ->join('artikel', 'kategori.id', '=', 'artikel.kategori_id')
+                ->join('foto_blog', 'artikel.id', '=', 'foto_blog.artikel_id')
+                ->where('kategori.kategori','like',"%".$cari."%")->get();
+        }
         
-    //     if(!empty($kategori[0]->id)){
-    //         $artikel[] = fractal($kategori, new SearchTransformer());
-    //     }else{
-    //         $tag = DB::table('tag')
-    //             ->join('artikel_tag', 'tag.id', '=', 'artikel_tag.tag_id')
-    //             ->join('artikel', 'artikel.id', '=', 'artikel_tag.artikel_id')
-    //             ->join('kategori', 'artikel.kategori_id','=','kategori.id')
-    //             ->join('foto_blog', 'artikel.id', '=', 'foto_blog.artikel_id')
-    //             ->where('nama_tag','like','%'.$cari.'%')->get();
-    //     }
+        if(!empty($kategori[0]->id)){
+            $artikel[] = fractal($kategori, new SearchTransformer());
+        }else{
+            $tag = DB::table('tag')
+                ->join('artikel_tag', 'tag.id', '=', 'artikel_tag.tag_id')
+                ->join('artikel', 'artikel.id', '=', 'artikel_tag.artikel_id')
+                ->join('kategori', 'artikel.kategori_id','=','kategori.id')
+                ->join('foto_blog', 'artikel.id', '=', 'foto_blog.artikel_id')
+                ->where('nama_tag','like','%'.$cari.'%')->get();
+        }
 
-    //     if(!empty($tag[0]->id)){
-    //         $artikel[] = fractal($tag, new SearchTransformer());
-    //     }
+        if(!empty($tag[0]->id)){
+            $artikel[] = fractal($tag, new SearchTransformer());
+        }
 
-    //     if(empty($artikel)){
-    //         $artikel = "Pencarian Tidak Ditemukan";
-    //     }
+        if(empty($artikel)){
+            $artikel = "Pencarian Tidak Ditemukan";
+        }
 
-    //     return reponse()->json($artikel);
+        return reponse()->json($artikel);
     }
 
 
