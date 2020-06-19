6dab774a84d8663883e3bc4293f66331507470a5
mkevin21 <kodingindie12@gmail.com>
2020-06-19 16:35:55 +0700

    perbaikan token api fix

--- untoldstories/app/Http/Controllers/Api/UsersController.php
+++ untoldstories/app/Http/Controllers/Api/UsersController.php
@@ -4,10 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\User;
-use App\Transformer\UserTransformer;
 use Auth;
-
-
+use App\Transformer\UserTransformer;
+use App\Http\Controllers\Controller;
 
 class UsersController extends Controller
 {
@@ -17,28 +16,33 @@ class UsersController extends Controller
         return response()->json($users); 
     }
 
-    public function register(Request $request, User $user){
-    	$this->validate($request, [
+    public function registers(Request $request, User $user){
+        $this->validate($request, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:2', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
 
-        $users = User::create([
-        				'name' => $request->name,
-        				'email' => $request->email,
-        				'api_token' => bcrypt($request->email), 
-        				'password' => bcrypt($request->password),
-        		]);
-        return fractal()
-        		->item($users)
-        		->transformWith(new UserTransformer)
-        		->addMeta(['token' => $users->api_token])
-        		->toArray(); 
+        $users = $user->create([
+            "name"      => $request->name,
+            "email"     => $request->email,
+            "password"  => bcrypt($request->password),
+            "api_token" => bcrypt($request->email)
+        ]);
+
+        $respon = fractal()
+            ->item($users)
+            ->transformWith(new UserTransformer)
+            ->addMeta([
+                'token' => $users->api_token
+            ])
+            ->toArray();
+        
+        return response()->json($respon, 201);
     }
 
     public function login(Request $request, User $user){
-    	if(!Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+        if(!Auth::attempt(['email' => $request->email, 'password' => $request->password])){
             return response()->json(['error' => 'email dan password tidak cocok', 401]);
         }
 
@@ -51,4 +55,3 @@ class UsersController extends Controller
             ])
             ->toArray();
     }
-}
